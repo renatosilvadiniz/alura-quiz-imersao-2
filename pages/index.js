3f62bfa8cc0f4a00c0cf563b5c4bdbe5ef440471
Renato Diniz <renatosilvadiniz@hotmail.com>
2021-01-25 19:51:16 -0300

    Aula 1 - 100%

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from "../db.json";
+import BackgroundImage from "../components/BackgroundImage";
+import QuizContainer from "../components/QuizContainer";
+import Widget from "../components/Widget";
+import QuizLogo from "../components/QuizLogo";
+import Footer from "../components/Footer";
+import GitHubCorner from "../components/GitHubCorner";
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage backgroundImage={db.bg}>
+      <GitHubCorner />
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+    </BackgroundImage>
+  );
 }
